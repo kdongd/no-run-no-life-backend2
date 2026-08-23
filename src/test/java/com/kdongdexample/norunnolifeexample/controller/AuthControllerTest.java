0189387927a26536b8fe3e7c6cad06f84bc70284
kdongd <kimdongd2@naver.com>
2026-08-23 09:25:42 +0900

    test: AuthControllerTest의 @MockBean을 @MockitoBean으로 교체 (deprecated API 정리)

--- src/test/java/com/kdongdexample/norunnolifeexample/controller/AuthControllerTest.java
+++ src/test/java/com/kdongdexample/norunnolifeexample/controller/AuthControllerTest.java
@@ -15,9 +15,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -36,7 +36,7 @@ class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private AuthService authService;
 
     @Test
